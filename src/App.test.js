1aa7f290cf0404c72916b2ed9d4355f4ce1bb7f7
AuroraRW <aurorawangca@gmail.com>
2026-03-15 22:27:29 -0600

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react cicd app', () => {
   render(<App />);
   const linkElement = screen.getByText(/react cicd app/i);
   expect(linkElement).toBeInTheDocument();
